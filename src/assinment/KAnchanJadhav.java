4838a83dfe5c31026f9fbe6399ffc855face44c3
KanchanJadhav13 <kanchanwavhale2013@gmail.com>
2024-01-14 21:19:50 +0530

    [KAnchan]:Add new class

---
+++ src/assinment/KAnchanJadhav.java
@@ -0,0 +1,11 @@
+package assinment;
+
+public class KAnchanJadhav {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hii this is Kanchan");
+	}
+
+}
